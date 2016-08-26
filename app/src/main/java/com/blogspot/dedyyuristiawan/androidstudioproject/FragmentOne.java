708beea444c61f14af0690b1822590d0032f7edd
Dedy Yuristiawan <dedyyuristiawan@Dedys-MacBook-Pro.local>
2016-08-27 02:55:35 +0700

    For All trick and tutorial about android development. You can visit on my blog. Dedyyuristiawan.blogspot.com
    1. Navigation Drawer.

---
+++ app/src/main/java/com/blogspot/dedyyuristiawan/androidstudioproject/FragmentOne.java
@@ -0,0 +1,22 @@
+package com.blogspot.dedyyuristiawan.androidstudioproject;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by dedyyuristiawan on 8/27/16.
+ */
+public class FragmentOne extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_one, container, false);
+
+        return rootView;
+    }
+}
